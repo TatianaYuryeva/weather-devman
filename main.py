2c5e639b33557c9f650a1c042c1114dad2374e97
TatianaYuryeva <91006136+TatianaYuryeva@users.noreply.github.com>
2024-03-11 13:17:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import requests
 
-locations = ('Лондон', 'аэропорт Шереметьево', 'Череповец')
-url = 'https://wttr.in/'
-payload = {
-    "lang": "ru",
-    "MmnqT": "",
-}
 
-
-def fetch_weather(locations, url, payload):
-    weather_list = []
-    for location in locations:
-        response = requests.get(f'{url}{location}', params=payload)
-        response.raise_for_status()
-        weather_list.append(response.text)
-    return weather_list
-
-
-def print_weather():
-    for weather in fetch_weather(locations, url, payload):
-        print(weather)
+def fetch_weather(location, payload):
+    url = 'https://wttr.in/'
+    response = requests.get(f'{url}{location}', params=payload)
+    response.raise_for_status()
+    return response.text
 
 
 if __name__ == '__main__':
-    print_weather()
+    locations = ['Лондон', 'svo', 'Череповец']
+    payload = {
+        "lang": "ru",
+        "MnqT": "",
+    }
+
+    for location in locations:
+        print(fetch_weather(location, payload))
